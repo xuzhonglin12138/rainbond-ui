5505e23fdb3e37931d0ca5378b570d5703c42444
zsl1549 <zsl1549@163.com>
2021-06-08 09:39:44 +0800

    Merge remote-tracking branch 'rainbond/master' into cloud

--- src/services/team.js
+++ src/services/team.js
@@ -362,6 +362,7 @@ export async function getTeamRegionGroups(body = {}, handleError) {
         query: body.query,
         region_name: body.region_name
       },
+      noModels: body.noModels,
       handleError
     }
   );
