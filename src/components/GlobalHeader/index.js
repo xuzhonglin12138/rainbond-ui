be14b957eeb8ef63f6ce53fc076df3fdb0504b9a
zsl1549 <zsl1549@163.com>
2021-06-28 18:47:02 +0800

    Fix   the  logoutEnable

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -24,7 +24,6 @@ import ChangePassword from '../ChangePassword';
 import NoticeIcon from '../NoticeIcon';
 import styles from './index.less';
 
-
 const { Header } = Layout;
 @connect(({ user, global, appControl, order }) => ({
   rainbondInfo: global.rainbondInfo,
@@ -255,7 +254,8 @@ export default class GlobalHeader extends PureComponent {
         <Menu selectedKeys={[]} onClick={this.handleMenuClick}>
           {MenuItems('userCenter', handleUserSvg, '个人中心')}
           {MenuItems('cpw', handleEditSvg, '修改密码')}
-          {MenuItems('logout', handleLogoutSvg, '退出登录')}
+          {!rainbondUtil.logoutEnable(rainbondInfo) &&
+            MenuItems('logout', handleLogoutSvg, '退出登录')}
         </Menu>
       </div>
     );
