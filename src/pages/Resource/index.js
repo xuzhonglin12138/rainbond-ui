2eeb2b411ffa0a508a4a734655e6421d7065aa5a
xuzl <guox@MacBook-Pro-For-Xuzl.local>
2022-11-14 10:48:33 +0800

    fix: yaml update bug

--- src/pages/Resource/index.js
+++ src/pages/Resource/index.js
@@ -10,6 +10,7 @@ import CodeMirrorForm from '../../components/CodeMirrorForm';
 import { getKubernetesVal, getSingleKubernetesVal, addSingleKubernetesVal, delSingleKubernetesVal, editSingleKubernetesVal } from "../../services/application";
 import ConfirmModal from "../../components/ConfirmModal";
 import styles from './index.less';
+import LoginTab from '@/components/Login/LoginTab';
 
 @Form.create()
 class Index extends PureComponent {
@@ -81,13 +82,33 @@ class Index extends PureComponent {
   };
   // 编辑
   editButton = (val, row) => {
-    this.setState({
-      type: val,
-      visible: true,
-      editName: row.name,
-      localContent: row.content,
-      editId: row.ID,
-      title: formatMessage({id:'addKubenetesResource.table.btn.edit'})
+    const teamName = globalUtil.getCurrTeamName();
+    const app_id = globalUtil.getAppID();
+    getSingleKubernetesVal({
+      team_name: teamName,
+      app_id: app_id,
+      list_name:row.name,
+      id:row.ID
+    }).then(res =>{
+      if (res && res.response_data && res.response_data.code == 200) {
+        this.setState({
+          type: val,
+          visible: true,
+          editName: row.name,
+          localContent: res.list.content,
+          editId: row.ID,
+          title: formatMessage({id:'addKubenetesResource.table.btn.edit'})
+        })
+      }else{
+        this.setState({
+          type: val,
+          visible: true,
+          editName: row.name,
+          localContent: row.content,
+          editId: row.ID,
+          title: formatMessage({id:'addKubenetesResource.table.btn.edit'})
+        })
+      }
     })
   }
   editErrButton = (val, row) => {
@@ -328,7 +349,6 @@ class Index extends PureComponent {
             </div>
           ): (
               <Table dataSource = { content } columns = { columns } />
-
             )}
 
         </Card>
